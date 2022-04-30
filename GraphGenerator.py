a90dfffffb72b791d273044c2f1ba28cd0a615e5
Ron <ronisr@post.bgu.ac.il>
2022-04-30 17:08:47 +0300

    First Commit

--- GraphGenerator.py
+++ GraphGenerator.py
@@ -5,29 +5,20 @@ from Parser.CodeFromFile import CodeFromFile
 
 # pip install gitpython
 
+files_path= './Files'
+codes_path = files_path+'/codes/'
+graphs_path = files_path+'/graphs/'
 
-def generate_path_to_code(pathToCode :string) -> string:
-    if 'git' in pathToCode:
-        projectName = os.path.basename(pathToCode)
-        projectName = projectName[0:projectName.rindex('.')]
-        sourcePath: string = 'Files/codes/' + projectName
-        return sourcePath
-    else: return pathToCode
+def init_folders():
+    for path in [files_path, codes_path, graphs_path]:
+        isExist = os.path.exists(path)
+        if not isExist:
+            os.makedirs(path)
 
-def generate_path_to_graph(pathToCode :string) -> string:
-    projectName: string = os.path.basename(pathToCode)
-    outputPath = 'Files/graphs/' + projectName + '.json'
-    return outputPath
-
-def generate_graph_from_git_project(gitPath: string) -> string:
-    codePath = generate_path_to_code(gitPath)
-    graphPath = generate_path_to_graph(codePath)
-    if not os.path.exists(codePath):
-        clone_project_from_git(gitPath, codePath)
-    # if not os.path.exists(graphPath):
-    code_to_graph(codePath, graphPath)
-    return graphPath
-
+def get_project_name(git_url):
+    projectName = os.path.basename(git_url)
+    projectName = projectName[0:projectName.rindex('.')]
+    return projectName
 
 def code_to_graph(codePath :string, outputPath :string) -> string:
     projectName: string = os.path.basename(codePath)
@@ -40,3 +31,21 @@ def clone_project_from_git(gitPath: string, outputPath :string) -> None:
             Repo.clone_from(gitPath, outputPath)
         except:
             print("git project hasn't found. try again.")
+
+
+class GraphGenerator:
+
+    def __init__(self):
+        init_folders()
+
+    def generate_graph_from_git_project(self, gitPath: string) -> string:
+        project_name = get_project_name(gitPath)
+        codePath = codes_path + project_name
+        graphPath = graphs_path + project_name + '.json'
+
+        if not os.path.exists(codePath):
+            clone_project_from_git(gitPath, codePath)
+            code_to_graph(codePath, graphPath)
+        return graphPath
+
+
--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import string
-from GraphGenerator import generate_graph_from_git_project, code_to_graph
-
-projects = [
-    "https://github.com/waz11/src1.git"
-    # "https://github.com/waz11/AES.git",
-    # "https://github.com/waz11/GUI_maze.git",
-    # "https://github.com/Adarsh9616/Electricity_Billing_System.git",
-    # "https://github.com/chabedalam11/Exam-Seating-Arrangement-System-Using-JSP-Servlet.git",
-    # "https://github.com/meetakbari/CV-Resume-Builder.git"
-]
-
-def main():
-    for project in projects:
-        graphPath :string = generate_graph_from_git_project(project)
-
-    # code_to_graph('./Files/codes/src1', 'Files/graphs/src1.json')
-
+from GraphGenerator import GraphGenerator
 
 if __name__ == '__main__':
-    main()
+
+    generator = GraphGenerator()
+    with open('corpus.txt') as file:
+        corpus = [line.rstrip() for line in file]
+    for url in corpus: generator.generate_graph_from_git_project(url)
 
